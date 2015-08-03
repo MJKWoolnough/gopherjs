c8155ce2e535a862c30271134e3fe96a784f7c49
Michael Woolnough <michael.woolnough@gmail.com>
2015-08-03 12:33:25 +0100

    Created simple js based progress bar

---
+++ progress/progress.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"image/color"
+	"io"
+	"strconv"
+
+	"github.com/MJKWoolnough/gopherjs/xjs"
+
+	"honnef.co/go/js/dom"
+)
+
+type ProgressBar struct {
+	*dom.HTMLCanvasElement
+	percent                int
+	ctx                    *dom.CanvasRenderingContext2D
+	width, height          int
+	foreground, background string
+}
+
+func (p *ProgressBar) Percent(i int) {
+	p.percent = i
+	p.draw()
+}
+
+func (p *ProgressBar) draw() {
+	p.ctx.FillStyle = p.background
+	p.ctx.FillRect(0, 0, p.width, p.height)
+	p.ctx.FillStyle = p.foreground
+	p.ctx.FillRect(0, 0, p.width*p.percent/100, p.height)
+}
+
+func NewProgressBar(fore, back color.Color, width, height int) *ProgressBar {
+	c := xjs.CreateElement("canvas").(*dom.HTMLCanvasElement)
+	c.SetAttribute("width", strconv.Itoa(width))
+	c.SetAttribute("height", strconv.Itoa(height))
+	r, g, b, _ := fore.RGBA()
+	foreground := "rgb(" + strconv.Itoa(int(r>>8)) + ", " + strconv.Itoa(int(g>>8)) + ", " + strconv.Itoa(int(b>>8)) + ")"
+	r, g, b, _ = back.RGBA()
+	background := "rgb(" + strconv.Itoa(int(r>>8)) + ", " + strconv.Itoa(int(g>>8)) + ", " + strconv.Itoa(int(b>>8)) + ")"
+	pb := &ProgressBar{
+		HTMLCanvasElement: c,
+		ctx:               c.GetContext2d(),
+		width:             width,
+		height:            height,
+		foreground:        foreground,
+		background:        background,
+	}
+	pb.draw()
+	return pb
+}
+
+type ProgressReader struct {
+	*ProgressBar
+	io.Reader
+	offset, size int
+}
+
+func (p *ProgressBar) Reader(r io.Reader, size int) *ProgressReader {
+	return &ProgressReader{p, r, 0, size}
+}
+
+func (pr ProgressReader) Read(p []byte) (int, error) {
+	n, err := pr.Reader.Read(p)
+	pr.offset += n
+	pr.Percent(100 * pr.offset / pr.size)
+	return n, err
+}
