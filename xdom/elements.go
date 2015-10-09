be3b6f3247b10a224c58567c4f3b0367ce9d3c9a
Michael Woolnough <michael.woolnough@gmail.com>
2015-10-09 20:55:03 +0100

    Added generated elements.go

---
+++ xdom/elements.go
@@ -0,0 +1,634 @@
+// Package xdom provides simple functions to create HTML elements with the correct type
+package xdom
+
+// File automatically generated with ./genMap.sh
+
+import (
+	"github.com/MJKWoolnough/gopherjs/xjs"
+	"honnef.co/go/js/dom"
+)
+
+// A returns an a with type *dom.HTMLAnchorElement
+func A() *dom.HTMLAnchorElement {
+	return xjs.CreateElement("a").(*dom.HTMLAnchorElement)
+}
+
+// Abbr returns an abbr with type dom.Element
+func Abbr() dom.Element {
+	return xjs.CreateElement("abbr")
+}
+
+// Acronym returns an acronym with type dom.Element
+func Acronym() dom.Element {
+	return xjs.CreateElement("acronym")
+}
+
+// Address returns an address with type dom.Element
+func Address() dom.Element {
+	return xjs.CreateElement("address")
+}
+
+// Applet returns an applet with type *dom.HTMLAppletElement
+func Applet() *dom.HTMLAppletElement {
+	return xjs.CreateElement("applet").(*dom.HTMLAppletElement)
+}
+
+// Area returns an area with type *dom.HTMLAreaElement
+func Area() *dom.HTMLAreaElement {
+	return xjs.CreateElement("area").(*dom.HTMLAreaElement)
+}
+
+// Article returns an article with type dom.Element
+func Article() dom.Element {
+	return xjs.CreateElement("article")
+}
+
+// Aside returns an aside with type dom.Element
+func Aside() dom.Element {
+	return xjs.CreateElement("aside")
+}
+
+// Audio returns an audio with type *dom.HTMLAudioElement
+func Audio() *dom.HTMLAudioElement {
+	return xjs.CreateElement("audio").(*dom.HTMLAudioElement)
+}
+
+// B returns an b with type dom.Element
+func B() dom.Element {
+	return xjs.CreateElement("b")
+}
+
+// Base returns an base with type *dom.HTMLBaseElement
+func Base() *dom.HTMLBaseElement {
+	return xjs.CreateElement("base").(*dom.HTMLBaseElement)
+}
+
+// Basefont returns an basefont with type dom.Element
+func Basefont() dom.Element {
+	return xjs.CreateElement("basefont")
+}
+
+// Bdi returns an bdi with type dom.Element
+func Bdi() dom.Element {
+	return xjs.CreateElement("bdi")
+}
+
+// Bdo returns an bdo with type dom.Element
+func Bdo() dom.Element {
+	return xjs.CreateElement("bdo")
+}
+
+// Big returns an big with type dom.Element
+func Big() dom.Element {
+	return xjs.CreateElement("big")
+}
+
+// Blockquote returns an blockquote with type *dom.HTMLQuoteElement
+func Blockquote() *dom.HTMLQuoteElement {
+	return xjs.CreateElement("blockquote").(*dom.HTMLQuoteElement)
+}
+
+// Body returns an body with type *dom.HTMLBodyElement
+func Body() *dom.HTMLBodyElement {
+	return xjs.CreateElement("body").(*dom.HTMLBodyElement)
+}
+
+// Br returns an br with type *dom.HTMLBRElement
+func Br() *dom.HTMLBRElement {
+	return xjs.CreateElement("br").(*dom.HTMLBRElement)
+}
+
+// Button returns an button with type *dom.HTMLButtonElement
+func Button() *dom.HTMLButtonElement {
+	return xjs.CreateElement("button").(*dom.HTMLButtonElement)
+}
+
+// Canvas returns an canvas with type *dom.HTMLCanvasElement
+func Canvas() *dom.HTMLCanvasElement {
+	return xjs.CreateElement("canvas").(*dom.HTMLCanvasElement)
+}
+
+// Caption returns an caption with type *dom.HTMLTableCaptionElement
+func Caption() *dom.HTMLTableCaptionElement {
+	return xjs.CreateElement("caption").(*dom.HTMLTableCaptionElement)
+}
+
+// Center returns an center with type dom.Element
+func Center() dom.Element {
+	return xjs.CreateElement("center")
+}
+
+// Cite returns an cite with type *dom.HTMLQuoteElement
+func Cite() *dom.HTMLQuoteElement {
+	return xjs.CreateElement("cite").(*dom.HTMLQuoteElement)
+}
+
+// Code returns an code with type dom.Element
+func Code() dom.Element {
+	return xjs.CreateElement("code")
+}
+
+// Col returns an col with type *dom.HTMLTableColElement
+func Col() *dom.HTMLTableColElement {
+	return xjs.CreateElement("col").(*dom.HTMLTableColElement)
+}
+
+// Colgroup returns an colgroup with type *dom.HTMLTableColElement
+func Colgroup() *dom.HTMLTableColElement {
+	return xjs.CreateElement("colgroup").(*dom.HTMLTableColElement)
+}
+
+// Content returns an content with type dom.Element
+func Content() dom.Element {
+	return xjs.CreateElement("content")
+}
+
+// Data returns an data with type *dom.HTMLDataElement
+func Data() *dom.HTMLDataElement {
+	return xjs.CreateElement("data").(*dom.HTMLDataElement)
+}
+
+// Datalist returns an datalist with type *dom.HTMLDataListElement
+func Datalist() *dom.HTMLDataListElement {
+	return xjs.CreateElement("datalist").(*dom.HTMLDataListElement)
+}
+
+// Dd returns an dd with type dom.Element
+func Dd() dom.Element {
+	return xjs.CreateElement("dd")
+}
+
+// Del returns an del with type *dom.HTMLModElement
+func Del() *dom.HTMLModElement {
+	return xjs.CreateElement("del").(*dom.HTMLModElement)
+}
+
+// Details returns an details with type dom.Element
+func Details() dom.Element {
+	return xjs.CreateElement("details")
+}
+
+// Dfn returns an dfn with type dom.Element
+func Dfn() dom.Element {
+	return xjs.CreateElement("dfn")
+}
+
+// Dialog returns an dialog with type dom.Element
+func Dialog() dom.Element {
+	return xjs.CreateElement("dialog")
+}
+
+// Dir returns an dir with type *dom.HTMLDirectoryElement
+func Dir() *dom.HTMLDirectoryElement {
+	return xjs.CreateElement("dir").(*dom.HTMLDirectoryElement)
+}
+
+// Div returns an div with type *dom.HTMLDivElement
+func Div() *dom.HTMLDivElement {
+	return xjs.CreateElement("div").(*dom.HTMLDivElement)
+}
+
+// Dl returns an dl with type *dom.HTMLDListElement
+func Dl() *dom.HTMLDListElement {
+	return xjs.CreateElement("dl").(*dom.HTMLDListElement)
+}
+
+// Dt returns an dt with type dom.Element
+func Dt() dom.Element {
+	return xjs.CreateElement("dt")
+}
+
+// Em returns an em with type dom.Element
+func Em() dom.Element {
+	return xjs.CreateElement("em")
+}
+
+// Embed returns an embed with type *dom.HTMLEmbedElement
+func Embed() *dom.HTMLEmbedElement {
+	return xjs.CreateElement("embed").(*dom.HTMLEmbedElement)
+}
+
+// Fieldset returns an fieldset with type *dom.HTMLFieldSetElement
+func Fieldset() *dom.HTMLFieldSetElement {
+	return xjs.CreateElement("fieldset").(*dom.HTMLFieldSetElement)
+}
+
+// Figcaption returns an figcaption with type dom.Element
+func Figcaption() dom.Element {
+	return xjs.CreateElement("figcaption")
+}
+
+// Figure returns an figure with type dom.Element
+func Figure() dom.Element {
+	return xjs.CreateElement("figure")
+}
+
+// Font returns an font with type *dom.HTMLFontElement
+func Font() *dom.HTMLFontElement {
+	return xjs.CreateElement("font").(*dom.HTMLFontElement)
+}
+
+// Footer returns an footer with type dom.Element
+func Footer() dom.Element {
+	return xjs.CreateElement("footer")
+}
+
+// Form returns an form with type *dom.HTMLFormElement
+func Form() *dom.HTMLFormElement {
+	return xjs.CreateElement("form").(*dom.HTMLFormElement)
+}
+
+// Frame returns an frame with type *dom.HTMLFrameElement
+func Frame() *dom.HTMLFrameElement {
+	return xjs.CreateElement("frame").(*dom.HTMLFrameElement)
+}
+
+// Frameset returns an frameset with type *dom.HTMLFrameSetElement
+func Frameset() *dom.HTMLFrameSetElement {
+	return xjs.CreateElement("frameset").(*dom.HTMLFrameSetElement)
+}
+
+// H1 returns an h1 with type *dom.HTMLHeadingElement
+func H1() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h1").(*dom.HTMLHeadingElement)
+}
+
+// H2 returns an h2 with type *dom.HTMLHeadingElement
+func H2() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h2").(*dom.HTMLHeadingElement)
+}
+
+// H3 returns an h3 with type *dom.HTMLHeadingElement
+func H3() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h3").(*dom.HTMLHeadingElement)
+}
+
+// H4 returns an h4 with type *dom.HTMLHeadingElement
+func H4() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h4").(*dom.HTMLHeadingElement)
+}
+
+// H5 returns an h5 with type *dom.HTMLHeadingElement
+func H5() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h5").(*dom.HTMLHeadingElement)
+}
+
+// H6 returns an h6 with type *dom.HTMLHeadingElement
+func H6() *dom.HTMLHeadingElement {
+	return xjs.CreateElement("h6").(*dom.HTMLHeadingElement)
+}
+
+// Head returns an head with type *dom.HTMLHeadElement
+func Head() *dom.HTMLHeadElement {
+	return xjs.CreateElement("head").(*dom.HTMLHeadElement)
+}
+
+// Header returns an header with type dom.Element
+func Header() dom.Element {
+	return xjs.CreateElement("header")
+}
+
+// Hr returns an hr with type *dom.HTMLHRElement
+func Hr() *dom.HTMLHRElement {
+	return xjs.CreateElement("hr").(*dom.HTMLHRElement)
+}
+
+// Html returns an html with type *dom.HTMLHtmlElement
+func Html() *dom.HTMLHtmlElement {
+	return xjs.CreateElement("html").(*dom.HTMLHtmlElement)
+}
+
+// I returns an i with type dom.Element
+func I() dom.Element {
+	return xjs.CreateElement("i")
+}
+
+// Iframe returns an iframe with type *dom.HTMLIFrameElement
+func Iframe() *dom.HTMLIFrameElement {
+	return xjs.CreateElement("iframe").(*dom.HTMLIFrameElement)
+}
+
+// Img returns an img with type *dom.HTMLImageElement
+func Img() *dom.HTMLImageElement {
+	return xjs.CreateElement("img").(*dom.HTMLImageElement)
+}
+
+// Input returns an input with type *dom.HTMLInputElement
+func Input() *dom.HTMLInputElement {
+	return xjs.CreateElement("input").(*dom.HTMLInputElement)
+}
+
+// Ins returns an ins with type *dom.HTMLModElement
+func Ins() *dom.HTMLModElement {
+	return xjs.CreateElement("ins").(*dom.HTMLModElement)
+}
+
+// Kbd returns an kbd with type dom.Element
+func Kbd() dom.Element {
+	return xjs.CreateElement("kbd")
+}
+
+// Keygen returns an keygen with type *dom.HTMLKeygenElement
+func Keygen() *dom.HTMLKeygenElement {
+	return xjs.CreateElement("keygen").(*dom.HTMLKeygenElement)
+}
+
+// Label returns an label with type *dom.HTMLLabelElement
+func Label() *dom.HTMLLabelElement {
+	return xjs.CreateElement("label").(*dom.HTMLLabelElement)
+}
+
+// Legend returns an legend with type *dom.HTMLLegendElement
+func Legend() *dom.HTMLLegendElement {
+	return xjs.CreateElement("legend").(*dom.HTMLLegendElement)
+}
+
+// Li returns an li with type *dom.HTMLLIElement
+func Li() *dom.HTMLLIElement {
+	return xjs.CreateElement("li").(*dom.HTMLLIElement)
+}
+
+// Link returns an link with type *dom.HTMLLinkElement
+func Link() *dom.HTMLLinkElement {
+	return xjs.CreateElement("link").(*dom.HTMLLinkElement)
+}
+
+// Main returns an main with type dom.Element
+func Main() dom.Element {
+	return xjs.CreateElement("main")
+}
+
+// Map returns an map with type *dom.HTMLMapElement
+func Map() *dom.HTMLMapElement {
+	return xjs.CreateElement("map").(*dom.HTMLMapElement)
+}
+
+// Mark returns an mark with type dom.Element
+func Mark() dom.Element {
+	return xjs.CreateElement("mark")
+}
+
+// Menu returns an menu with type *dom.HTMLMenuElement
+func Menu() *dom.HTMLMenuElement {
+	return xjs.CreateElement("menu").(*dom.HTMLMenuElement)
+}
+
+// Menuitem returns an menuitem with type dom.Element
+func Menuitem() dom.Element {
+	return xjs.CreateElement("menuitem")
+}
+
+// Meta returns an meta with type *dom.HTMLMetaElement
+func Meta() *dom.HTMLMetaElement {
+	return xjs.CreateElement("meta").(*dom.HTMLMetaElement)
+}
+
+// Meter returns an meter with type *dom.HTMLMeterElement
+func Meter() *dom.HTMLMeterElement {
+	return xjs.CreateElement("meter").(*dom.HTMLMeterElement)
+}
+
+// Nav returns an nav with type dom.Element
+func Nav() dom.Element {
+	return xjs.CreateElement("nav")
+}
+
+// Noframes returns an noframes with type dom.Element
+func Noframes() dom.Element {
+	return xjs.CreateElement("noframes")
+}
+
+// Noscript returns an noscript with type dom.Element
+func Noscript() dom.Element {
+	return xjs.CreateElement("noscript")
+}
+
+// Object returns an object with type *dom.HTMLObjectElement
+func Object() *dom.HTMLObjectElement {
+	return xjs.CreateElement("object").(*dom.HTMLObjectElement)
+}
+
+// Ol returns an ol with type *dom.HTMLOListElement
+func Ol() *dom.HTMLOListElement {
+	return xjs.CreateElement("ol").(*dom.HTMLOListElement)
+}
+
+// Optgroup returns an optgroup with type *dom.HTMLOptGroupElement
+func Optgroup() *dom.HTMLOptGroupElement {
+	return xjs.CreateElement("optgroup").(*dom.HTMLOptGroupElement)
+}
+
+// Option returns an option with type *dom.HTMLOptionElement
+func Option() *dom.HTMLOptionElement {
+	return xjs.CreateElement("option").(*dom.HTMLOptionElement)
+}
+
+// Output returns an output with type *dom.HTMLOutputElement
+func Output() *dom.HTMLOutputElement {
+	return xjs.CreateElement("output").(*dom.HTMLOutputElement)
+}
+
+// P returns an p with type *dom.HTMLParagraphElement
+func P() *dom.HTMLParagraphElement {
+	return xjs.CreateElement("p").(*dom.HTMLParagraphElement)
+}
+
+// Param returns an param with type *dom.HTMLParamElement
+func Param() *dom.HTMLParamElement {
+	return xjs.CreateElement("param").(*dom.HTMLParamElement)
+}
+
+// Pre returns an pre with type *dom.HTMLPreElement
+func Pre() *dom.HTMLPreElement {
+	return xjs.CreateElement("pre").(*dom.HTMLPreElement)
+}
+
+// Progress returns an progress with type *dom.HTMLProgressElement
+func Progress() *dom.HTMLProgressElement {
+	return xjs.CreateElement("progress").(*dom.HTMLProgressElement)
+}
+
+// Q returns an q with type *dom.HTMLQuoteElement
+func Q() *dom.HTMLQuoteElement {
+	return xjs.CreateElement("q").(*dom.HTMLQuoteElement)
+}
+
+// Rp returns an rp with type dom.Element
+func Rp() dom.Element {
+	return xjs.CreateElement("rp")
+}
+
+// Rt returns an rt with type dom.Element
+func Rt() dom.Element {
+	return xjs.CreateElement("rt")
+}
+
+// Ruby returns an ruby with type dom.Element
+func Ruby() dom.Element {
+	return xjs.CreateElement("ruby")
+}
+
+// S returns an s with type dom.Element
+func S() dom.Element {
+	return xjs.CreateElement("s")
+}
+
+// Samp returns an samp with type dom.Element
+func Samp() dom.Element {
+	return xjs.CreateElement("samp")
+}
+
+// Script returns an script with type *dom.HTMLScriptElement
+func Script() *dom.HTMLScriptElement {
+	return xjs.CreateElement("script").(*dom.HTMLScriptElement)
+}
+
+// Section returns an section with type dom.Element
+func Section() dom.Element {
+	return xjs.CreateElement("section")
+}
+
+// Select returns an select with type *dom.HTMLSelectElement
+func Select() *dom.HTMLSelectElement {
+	return xjs.CreateElement("select").(*dom.HTMLSelectElement)
+}
+
+// Shadow returns an shadow with type dom.Element
+func Shadow() dom.Element {
+	return xjs.CreateElement("shadow")
+}
+
+// Small returns an small with type dom.Element
+func Small() dom.Element {
+	return xjs.CreateElement("small")
+}
+
+// Source returns an source with type *dom.HTMLSourceElement
+func Source() *dom.HTMLSourceElement {
+	return xjs.CreateElement("source").(*dom.HTMLSourceElement)
+}
+
+// Span returns an span with type *dom.HTMLSpanElement
+func Span() *dom.HTMLSpanElement {
+	return xjs.CreateElement("span").(*dom.HTMLSpanElement)
+}
+
+// Strike returns an strike with type dom.Element
+func Strike() dom.Element {
+	return xjs.CreateElement("strike")
+}
+
+// Strong returns an strong with type dom.Element
+func Strong() dom.Element {
+	return xjs.CreateElement("strong")
+}
+
+// Style returns an style with type *dom.HTMLStyleElement
+func Style() *dom.HTMLStyleElement {
+	return xjs.CreateElement("style").(*dom.HTMLStyleElement)
+}
+
+// Sub returns an sub with type dom.Element
+func Sub() dom.Element {
+	return xjs.CreateElement("sub")
+}
+
+// Summary returns an summary with type dom.Element
+func Summary() dom.Element {
+	return xjs.CreateElement("summary")
+}
+
+// Sup returns an sup with type dom.Element
+func Sup() dom.Element {
+	return xjs.CreateElement("sup")
+}
+
+// Table returns an table with type *dom.HTMLTableElement
+func Table() *dom.HTMLTableElement {
+	return xjs.CreateElement("table").(*dom.HTMLTableElement)
+}
+
+// Tbody returns an tbody with type *dom.HTMLTableSectionElement
+func Tbody() *dom.HTMLTableSectionElement {
+	return xjs.CreateElement("tbody").(*dom.HTMLTableSectionElement)
+}
+
+// Td returns an td with type *dom.HTMLTableDataCellElement
+func Td() *dom.HTMLTableDataCellElement {
+	return xjs.CreateElement("td").(*dom.HTMLTableDataCellElement)
+}
+
+// Template returns an template with type dom.Element
+func Template() dom.Element {
+	return xjs.CreateElement("template")
+}
+
+// Textarea returns an textarea with type *dom.HTMLTextAreaElement
+func Textarea() *dom.HTMLTextAreaElement {
+	return xjs.CreateElement("textarea").(*dom.HTMLTextAreaElement)
+}
+
+// Tfoot returns an tfoot with type *dom.HTMLTableSectionElement
+func Tfoot() *dom.HTMLTableSectionElement {
+	return xjs.CreateElement("tfoot").(*dom.HTMLTableSectionElement)
+}
+
+// Th returns an th with type *dom.HTMLTableHeaderCellElement
+func Th() *dom.HTMLTableHeaderCellElement {
+	return xjs.CreateElement("th").(*dom.HTMLTableHeaderCellElement)
+}
+
+// Thead returns an thead with type *dom.HTMLTableSectionElement
+func Thead() *dom.HTMLTableSectionElement {
+	return xjs.CreateElement("thead").(*dom.HTMLTableSectionElement)
+}
+
+// Time returns an time with type *dom.HTMLTimeElement
+func Time() *dom.HTMLTimeElement {
+	return xjs.CreateElement("time").(*dom.HTMLTimeElement)
+}
+
+// Title returns an title with type *dom.HTMLTitleElement
+func Title() *dom.HTMLTitleElement {
+	return xjs.CreateElement("title").(*dom.HTMLTitleElement)
+}
+
+// Tr returns an tr with type *dom.HTMLTableRowElement
+func Tr() *dom.HTMLTableRowElement {
+	return xjs.CreateElement("tr").(*dom.HTMLTableRowElement)
+}
+
+// Track returns an track with type *dom.HTMLTrackElement
+func Track() *dom.HTMLTrackElement {
+	return xjs.CreateElement("track").(*dom.HTMLTrackElement)
+}
+
+// Tt returns an tt with type dom.Element
+func Tt() dom.Element {
+	return xjs.CreateElement("tt")
+}
+
+// U returns an u with type dom.Element
+func U() dom.Element {
+	return xjs.CreateElement("u")
+}
+
+// Ul returns an ul with type *dom.HTMLUListElement
+func Ul() *dom.HTMLUListElement {
+	return xjs.CreateElement("ul").(*dom.HTMLUListElement)
+}
+
+// Var returns an var with type dom.Element
+func Var() dom.Element {
+	return xjs.CreateElement("var")
+}
+
+// Video returns an video with type *dom.HTMLVideoElement
+func Video() *dom.HTMLVideoElement {
+	return xjs.CreateElement("video").(*dom.HTMLVideoElement)
+}
+
+// Wbr returns an wbr with type dom.Element
+func Wbr() dom.Element {
+	return xjs.CreateElement("wbr")
+}
