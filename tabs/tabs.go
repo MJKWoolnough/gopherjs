d05870268548bba42aa1119214421dd9f7890986
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-07 11:24:28 +0000

    Corrected type assertion

--- tabs/tabs.go
+++ tabs/tabs.go
@@ -134,7 +134,7 @@ func MakeTabs(t []Tab) dom.Node {
 				for _, tab := range tabs {
 					tab.Class().Remove("selected")
 				}
-				newContents := contents.CloneNode(false).(dom.Element)
+				newContents := contents.CloneNode(false).(*dom.HTMLDivElement)
 				contents.ParentNode().ReplaceChild(newContents, contents)
 				contents = newContents
 				tabs[i].Class().Add("selected")
