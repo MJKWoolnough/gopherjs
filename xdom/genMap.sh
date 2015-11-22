eec867fad181536ea9ab0fcb8e77490641c8e4d4
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-22 10:31:51 +0000

    Removed xjs import

--- xdom/genMap.sh
+++ xdom/genMap.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 (
-	echo "// Package xdom provides simple functions to create HTML elements with the correct type";
-	echo "package xdom";
-	echo "";
-	echo "// File automatically generated with ./genMap.sh";
-	echo "";
-	echo "import ("
-	echo "	\"github.com/MJKWoolnough/gopherjs/xjs\"";
-	echo "	\"honnef.co/go/js/dom\"";
-	echo ")";
+	cat <<HEREDOC
+// Package xdom provides simple functions to create HTML elements with the correct type
+package xdom
 
+// File automatically generated with ./genMap.sh
+
+import "honnef.co/go/js/dom"
+HEREDOC
 	while read element; do
 		interface="dom.Element";
 		if [ -z "${element/*:*/}" ]; then
@@ -21,7 +19,7 @@
 		echo "";
 		echo "// $cElement returns a \"$element\" element with type $interface"
 		echo "func $cElement() $interface {";
-		echo -n "	return xjs.CreateElement(\"$element\")";
+		echo -n "	return dom.GetWindow().Document().CreateElement(\"$element\")";
 		if [ "$interface" == "dom.Element" ]; then
 			echo;
 		else
