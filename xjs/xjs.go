4c2f41a2a990447d3b436bc6810479841acf77e6
Michael Woolnough <michael.woolnough@gmail.com>
2015-11-23 15:47:09 +0000

    Replaced some implementations with a more 'pure' javascript approach

--- xjs/xjs.go
+++ xjs/xjs.go
@@ -5,25 +5,36 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/gopherjs/gopherjs/js"
+
 	"honnef.co/go/js/dom"
 )
 
-var docNode = dom.GetWindow().Document()
+// body var used to cache the wrapped body once Body is sucessfully called
+var body *dom.HTMLBodyElement
 
 // Get the document's body element
-func Body() dom.HTMLElement {
-	return docNode.(dom.HTMLDocument).Body()
+func Body() *dom.HTMLBodyElement {
+	if body == nil {
+		o := js.Global.Get("document").Get("body")
+		if o == nil || o == js.Undefined {
+			return nil
+		}
+		body = &dom.HTMLBodyElement{BasicHTMLElement: &dom.BasicHTMLElement{&dom.BasicElement{&dom.BasicNode{js.Global.Get("document").Get("body")}}}}
+	}
+	return body
 }
 
 // DocumentFragment returns a new DocumentFragment as a dom.Node
-func DocumentFragment() dom.Node {
-	return dom.WrapNode(docNode.Underlying().Call("createDocumentFragment"))
+func DocumentFragment() *dom.BasicHTMLElement {
+	return &dom.BasicHTMLElement{&dom.BasicElement{&dom.BasicNode{js.Global.Get("document").Call("createDocumentFragment")}}}
 }
 
 // RemoveChildren removes all of the child nodes of the node given
 func RemoveChildren(node dom.Node) dom.Node {
-	for node.HasChildNodes() {
-		node.RemoveChild(node.LastChild())
+	n := node.Underlying()
+	for n.Call("hasChildNodes").Bool() {
+		n.Call("removeChild", n.Get("lastChild"))
 	}
 	return node
 }
@@ -31,36 +42,42 @@ func RemoveChildren(node dom.Node) dom.Node {
 // SetInnerText removes all child nodes from the given node and sets a single
 // Text Node with the given string
 func SetInnerText(node dom.Node, text string) dom.Node {
-	RemoveChildren(node)
-	node.AppendChild(Text(text))
+	n := node.Underlying()
+	for n.Call("hasChildNodes").Bool() {
+		n.Call("removeChild", n.Get("lastChild"))
+	}
+	n.Call("appendChild", js.Global.Get("document").Call("createTextNode", text))
 	return node
 }
 
 // SetPreText does similar to SetInnerText, but linebreaks are converted to <br />s
 func SetPreText(node dom.Node, text string) dom.Node {
-	RemoveChildren(node)
-	for n, part := range strings.Split(text, "\n") {
-		if n > 0 {
-			node.AppendChild(CreateElement("br"))
+	n := node.Underlying()
+	for n.Call("hasChildNodes").Bool() {
+		n.Call("removeChild", n.Get("lastChild"))
+	}
+	for i, part := range strings.Split(text, "\n") {
+		if i > 0 {
+			n.Call("appendChild", js.Global.Get("document").Call("createElement", "br"))
 		}
-		node.AppendChild(Text(part))
+		n.Call("appendChild", js.Global.Get("document").Call("createTextNode", part))
 	}
 	return node
 }
 
 // CreateElement is a shortcut to create an element with the given name
 func CreateElement(name string) dom.Element {
-	return dom.GetWindow().Document().CreateElement(name)
+	return dom.WrapElement(js.Global.Get("document").Call("createElement", name))
 }
 
 // Alert provides for formated alert boxes
 func Alert(format string, params ...interface{}) {
-	dom.GetWindow().Alert(fmt.Sprintf(format, params...))
+	js.Global.Get("document").Call("alert", fmt.Sprintf(format, params...))
 }
 
 // TextNode creates a text node containing the givin text
 func Text(text string) *dom.Text {
-	return docNode.CreateTextNode(text)
+	return &dom.Text{&dom.BasicNode{js.Global.Get("document").Call("createTextNode", text)}}
 }
 
 // AppendChildren appends all the given children to the parent.
