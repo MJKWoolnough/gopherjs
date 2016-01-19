85c28833f9504e503cfab036b328f7abbb8f3f33
Michael Woolnough <michael.woolnough@gmail.com>
2016-01-19 18:16:45 +0000

    start of rewrite to make better use of built-in JSON

--- json/encoder.go
+++ json/encoder.go
@@ -6,60 +6,51 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+type objectInfo struct {
+	v reflect.Value
+	c int
+}
+
 func Encode(v interface{}) string {
-	return js.Global.Get("JSON").Get("stringify").Invoke(processType(reflect.ValueOf(v))).String()
-}
-
-func processStruct(v reflect.Value) *js.Object {
-	t := v.Type()
-	o := js.Global.Get("Object").New()
-	for i := 0; i < t.NumField; i++ {
-		f := t.Field(i)
-		if f.PkgPath == "" { // exported
-			name := f.Name
-			switch tag := f.Tag.Get("json"); tag {
-			case "":
-			case "-":
-				continue
-			default:
-				// set name and check tag options
+	objects := make([]objectInfo, 1, 16) // worth working out actual maximum depth?
+	objects[0] = objectInfo{v: reflect.ValueOf(v)}
+	return js.Global.Get("JSON").Get("stringify").Invoke(v, func(key, value *js.Object) *js.Object {
+		v := objects[len(objects)-1].v
+		objects[len(objects)-1].c--
+		if objects[len(objects)-1].c == 0 {
+			objects = objects[:len(objects)-1]
+		}
+		switch k := v.Kind(); k {
+		case reflect.Struct:
+			v = v.FieldByName(key.String())
+		case reflect.Array, reflect.Slice:
+			v = v.Index(key.Int())
+		case reflect.Map:
+			v = v.MapIndex(reflect.ValueOf(key.String()))
+		}
+		switch k := v.Kind(); k {
+		case reflect.Struct:
+			c := 0
+			nf := v.NumField()
+			t := v.Type()
+			for i := 0; i < nf; i++ {
+				field := t.Field(i)
+				if field.PkgPath == "" {
+					tag := field.Tag.Get("json")
+					if tag == "-" {
+						value.Set(field.Name, js.Undefined)
+						value.Delete(field.Name)
+						continue
+					}
+					c++
+				}
 			}
-			o.Set(name, processType(v.Field(i)))
+			objects = append(objects, objectInfo{v, c})
+		case reflect.Array, reflect.Slice:
+			objects = append(objects, objectInfo{v, v.Len()})
+		case reflect.Map:
+			objects = append(objects, objectInfo{v, v.Len()})
 		}
-	}
-	return o
-}
-
-func processSlice(v reflect.Value) *js.Object {
-	a := js.Global.Get("Array").New(v.Len())
-	for i := 0; i < v.Len(); i++ {
-		a.SetIndex(i, processType(v.Index(i)))
-	}
-	return a
-}
-
-func processType(v reflect.Value) *js.Object {
-	for v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			return nil
-		}
-		v = v.Elem()
-	}
-	if v.IsNil() {
-		return nil
-	}
-	switch v.Kind() {
-	case reflect.Bool:
-		return js.Global.Get("Bool").Invoke(v.Interface())
-	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint, reflect.Uintptr, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int, reflect.Float32, reflect.Float64:
-		return js.Global.Get("Number").Invoke(v.Interface())
-	case reflect.String:
-		return js.Global.Get("String").Invoke(v.Interface())
-	case reflect.Struct:
-		return processStruct(v)
-	case reflect.Array, reflect.Slice:
-		return processSlice(v)
-	default:
-		return nil
-	}
+		return value
+	}).String()
 }
