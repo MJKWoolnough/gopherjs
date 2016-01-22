ab182b7bdffb8086a1e81c8dfdf65d1515bc1d55
Michael Woolnough <michael.woolnough@gmail.com>
2016-01-22 09:07:52 +0000

    added simple Encoder to write to a io.Writer

---
+++ json/stream.go
@@ -0,0 +1,22 @@
+package json
+
+import "io"
+
+type Encoder struct {
+	w   io.Writer
+	err error
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+func (e *Encoder) Encode(v interface{}) error {
+	b, err := Marshal(v)
+	if err != nil {
+		return err
+	}
+	_, err = e.w.Write(b)
+	e.err = err
+	return err
+}
