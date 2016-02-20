50d60b27ed5546e803788db63e445db900384f3a
Michael Woolnough <michael.woolnough@gmail.com>
2016-02-20 09:37:42 +0000

    implemented more of the calls

--- rpc/client.go
+++ rpc/client.go
@@ -1,29 +1,68 @@
 package rpc
 
-import "github.com/gopherjs/websocket"
+import (
+	"encoding/json"
 
-type Client struct {
-	nextID uint
+	"github.com/gopherjs/websocket"
+)
+
+type Call struct {
+	ServiceMethod string
+	Args, Reply   interface{}
+	Error         error
+	Done          chan *Call
 }
 
-func NewClient(addr string) *Client {
-	w, err := websocket.Dial(addr)
-}
-
-type call struct {
+type request struct {
 	Method string         `json:"method"`
 	ID     uint           `json:"id"`
 	Params [1]interface{} `json:"params"`
 }
 
+type Client struct {
+	ws     *websocket.Websocket
+	nextID uint
+	reqs   map[uint]func(json.RawMessage, error)
+}
+
+func NewClient(addr string) *Client {
+	w, err := websocket.New(addr)
+
+	return &Client{
+		ws:   w,
+		reqs: make(map[uint]func(json.RawMessage, error)),
+	}
+}
+
 func (c *Client) Call(method string, args interface{}, reply interface{}) error {
-	return nil
+	call := <-c.Go(method, args, reply, make(chan *Call, 1)).Done
+	return call.Error
 }
 
 func (c *Client) Close() error {
-	return nil
+	return c.w.Close()
 }
 
 func (c *Client) Go(method string, args interface{}, reply interface{}, done chan *Call) *Call {
-	return nil
+	call := &Call{
+		ServiceMethod: method,
+		Args:          args,
+		Reply:         reply,
+	}
+	if done == nil {
+		call.Done = make(chan *Call, 1)
+	} else {
+		call.Done = done
+	}
+	c.nextID++
+	id := c.nextID
+	c.reqs[id] = func(rm json.RawMessage, err error) {
+		if err != nil {
+			call.Error = err
+		} else if err = json.Unmarshal(rm, reply); err != nil {
+			call.Error = err
+		}
+		call.Done <- call
+	}
+	return call
 }
